8b580ccc3d6800f7ae381126c1c82ff75bbacae5
zhjch05 <zjc1996@brandeis.edu>
2015-06-18 13:56:06 +0000

    test socket

---
+++ client/pages/testsocket.js
@@ -0,0 +1,8 @@
+// Template.testsocket.rendered = function(){
+//     var io = Npm.require('socket.io-client');
+//     var socket = io('http://localhost');
+//     socket.on('news', function (data) {
+//     console.log(data);
+//     socket.emit('my other event', { my: 'data' });
+//     });
+// }
---
+++ server/startup.js
@@ -0,0 +1,16 @@
+Meteor.startup(function () {
+ //  // note, io(<port>) will create a http server for you
+	// var io = Npm.require('socket.io')(8888);
+
+	// io.on('connection', function (socket) {
+	//   io.emit('this', { will: 'be received by everyone'});
+
+	//   socket.on('private message', function (from, msg) {
+	//     console.log('I received a private message by ', from, ' saying ', msg);
+	//   });
+
+	//   socket.on('disconnect', function () {
+	//     io.emit('user disconnected');
+	//   });
+	// });
+});
